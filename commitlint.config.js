d1b76ac5b3f00dfaae060a4de84607cdf28ad700
Aaron Brewer <spaceribs@gmail.com>
2022-10-01 17:42:59 +0000

    ci: add deps and deps-dev to allowed scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,21 @@
+const {
+  utils: { getProjects },
+} = require('@commitlint/config-nx-scopes');
+
 module.exports = {
   extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
+  rules: {
+    'scope-enum': async (ctx) => [
+      2,
+      'always',
+      [
+        'deps',
+        'deps-dev',
+        ...(await getProjects(
+          ctx,
+          ({ name, projectType }) => !name.includes('e2e')
+        )),
+      ],
+    ],
+  },
 };
