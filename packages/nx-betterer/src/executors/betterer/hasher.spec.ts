b58ca51d08e7c2b6060b4516adc1aec800c71353
Aaron Brewer <spaceribs@gmail.com>
2022-12-31 15:00:17 -0500

    test(nx-betterer): fixup tests for executor

--- packages/nx-betterer/src/executors/betterer/hasher.spec.ts
+++ packages/nx-betterer/src/executors/betterer/hasher.spec.ts
@@ -3,20 +3,36 @@ import { Hasher, HasherContext } from '@nrwl/devkit';
 import { bettererHasher } from './hasher';
 
 describe('bettererHasher', () => {
-    it('should generate hash', async () => {
-        const mockHasher: Hasher = {
-            hashTask: jest.fn().mockReturnValue({value: 'hashed-task'})
-        } as unknown as Hasher
-        const hash = await bettererHasher({
-            id: 'my-task-id',
-            target: {
-                project: 'proj',
-                target: 'target'
+  it('should generate hash', async () => {
+    const mockHasher: Hasher = {
+      hashFile: jest.fn().mockReturnValue('test123'),
+      hashTask: jest
+        .fn()
+        .mockReturnValue({ value: 'hashed-task', details: { test: 123 } }),
+    } as unknown as Hasher;
+
+    const hash = await bettererHasher(
+      {
+        projectRoot: 'root',
+        id: 'my-task-id',
+        target: {
+          project: 'proj',
+          target: 'target',
+        },
+        overrides: {},
+      },
+      {
+        hasher: mockHasher,
+        projectName: 'proj',
+        workspace: {
+          projects: {
+            proj: {
+              root: 'libs/proj',
             },
-            overrides: {}
-        }, {
-            hasher: mockHasher
-        } as unknown as HasherContext)
-        expect(hash).toEqual({value: 'hashed-task'})
-    })
-})
+          },
+        },
+      } as unknown as HasherContext
+    );
+    expect(hash).toMatchSnapshot();
+  });
+});
