c8092bd76c9868d80a86d1e5949361e776fb2e64
Aaron Brewer <spaceribs@gmail.com>
2023-02-26 08:43:34 -0500

    feat(nx-threejs): init threejs plugin

---
+++ packages/nx-threejs/README.md
@@ -0,0 +1,11 @@
+# nx-threejs
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build nx-threejs` to build the library.
+
+## Running unit tests
+
+Run `nx test nx-threejs` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/nx-threejs/src/generators/nx-threejs/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { NxThreejsGeneratorSchema } from './schema';
+
+describe('nx-threejs generator', () => {
+  let appTree: Tree;
+  const options: NxThreejsGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
