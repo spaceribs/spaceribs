c146647366306ad7adbcc972d8c223b114021d84
Aaron Brewer <spaceribs@gmail.com>
2023-06-21 10:18:35 -0400

    fix(nx-betterer): default to ci mode unless update option is passed to executor

--- packages/nx-betterer/src/executors/betterer/schema.d.ts
+++ packages/nx-betterer/src/executors/betterer/schema.d.ts
@@ -5,25 +5,16 @@ export type BettererExecutorSchema =
   | BettererExecutorSchemaUpdate;
 
 interface BettererExecutorSchemaBase {
-  ci?: false;
-  watch?: false;
-  update?: false;
-}
-
-interface BettererExecutorSchemaCi {
-  ci: true;
   watch?: false;
   update?: false;
 }
 
 interface BettererExecutorSchemaWatch {
-  ci?: false;
   watch: true;
   update?: false;
 }
 
 interface BettererExecutorSchemaUpdate {
-  ci?: false;
   watch?: false;
   update: true;
 }
