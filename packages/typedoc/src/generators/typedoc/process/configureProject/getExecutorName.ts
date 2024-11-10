b886203d71c7584996011ae7a9017dba7949a6fd
Aaron Brewer <spaceribs@gmail.com>
2024-11-10 15:23:08 -0500

    style: run prettier across all files

--- packages/typedoc/src/generators/typedoc/process/configureProject/getExecutorName.ts
+++ packages/typedoc/src/generators/typedoc/process/configureProject/getExecutorName.ts
@@ -1,4 +1,4 @@
-import * as pkg from '../../../../../package.json'
-import * as project from '../../../../../project.json'
+import * as pkg from '../../../../../package.json';
+import * as project from '../../../../../project.json';
 
-export const getExecutorName = (): string => `${pkg.name}:${project.name}`
+export const getExecutorName = (): string => `${pkg.name}:${project.name}`;
